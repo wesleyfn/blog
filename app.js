9cd61291a5fc2be6b75a18b4d01a7beda3e70360
wesleyfn <wesleyfnpa@gmail.com>
2023-11-21 02:55:26 -0300

    adicionando front-end

--- app.js
+++ app.js
@@ -5,11 +5,36 @@ var db = require('./db.js');
 app.set("engine ejs", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
+//Configurar middleware para servir arquivos estáticos
+app.use(express.static('public'));
+
 //rotas...
 app.get('/', function (req, res) {
-    db.query("SELECT * FROM Autor", function (err, dado) {
-        res.render('index.ejs', {
-            dado: dado
+    const sql = `
+        SELECT 
+            Artigo.id_artigo,
+            Artigo.titulo,
+            Artigo.conteudo,
+            Artigo.n_curtidas,
+            Autor.nome_autor,
+            GROUP_CONCAT(TipoArtigo.nome_tipo_artigo) as tipos_artigo
+        FROM 
+            Artigo 
+        INNER JOIN 
+            Autor ON Artigo.fk_autor = Autor.id_autor 
+        INNER JOIN 
+            ArtigoTipoAssociacao ON Artigo.id_artigo = ArtigoTipoAssociacao.fk_artigo
+        INNER JOIN 
+            TipoArtigo ON ArtigoTipoAssociacao.fk_tipo_artigo = TipoArtigo.id_tipo_artigo
+        GROUP BY 
+            Artigo.id_artigo;
+    `
+
+    db.query(sql, function (err, result) {
+        if (err) throw err;
+
+        res.render('home.ejs', {
+            data: result
         });
     });
 });
