0186b5b6a1ecd43eb8c93dc557b0ebb32cd6a450
wesleyfn <wesleyfnpa@gmail.com>
2023-11-26 17:41:35 -0300

    nova organização de rotas

--- app.js
+++ app.js
@@ -1,48 +1,20 @@
-var express = require("express");
-var app = express();
-var mysql = require('./db.js');
+const express = require('express');
+const app = express();
 
-app.set("engine ejs", "ejs");
-app.use(express.urlencoded({ extended: false }));
 
-//Configurar middleware para servir arquivos estáticos
+// Importando rotas
+const home_routes = require('./routes/home');
+
+
+// Utilizando as rotas
+app.use('/', home_routes);
+
+
+
+
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-
-//rotas...
-app.get('/', function (req, res) {
-    const sql = `
-    SELECT 
-        Article.article_title,
-        Article.article_likes,
-        User.user_name,
-        COUNT(Comment.id_comment) AS article_comments,
-        CASE 
-            WHEN diff_secs < 60 THEN CONCAT(diff_secs, ' segundos atrás')
-            WHEN diff_secs < 3600 THEN CONCAT(FLOOR(diff_secs / 60), ' minutos atrás')
-            ELSE CONCAT(FLOOR(diff_secs / 3600), ' horas atrás')
-        END AS time_posted
-    FROM Article
-    JOIN User ON Article.fk_user_creator = User.id_user
-    LEFT JOIN Comment ON Article.id_article = Comment.fk_article
-    JOIN (
-        SELECT 
-            TIMESTAMPDIFF(SECOND, Article.article_create_time, NOW()) AS diff_secs
-        FROM Article
-        WHERE Article.id_article = 1
-    ) AS subquery ON 1 = 1
-    GROUP BY Article.id_article;
-    `
-
-    mysql.query(sql, function (err, result) {
-        if (err) throw err;
-
-        res.render('base.ejs', {
-            data: result
-        });
-    });
-});
-
 app.listen(3000, () => {
-    console.log('SERVIDOR ATIVO, ACESSE http://localhost:3000');
+    console.log(`Servidor rodando! Link: http://localhost:3000`);
 });
---
+++ routes/home.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('../db');
+
+// Função para gerar uma SQL query ordenada de acordo com o sortBy
+function generate_SQL(sort_by) {
+    return `
+    SELECT 
+        A.article_title,
+        A.article_likes,
+        U.user_name,
+        COUNT(C.id_comment) AS article_comments,
+        TIMESTAMPDIFF(SECOND, article_create_time, NOW()) AS diff_secs
+    FROM Article AS A
+    JOIN User AS U ON A.fk_user_creator = U.id_user
+    LEFT JOIN Comment AS C ON A.id_article = C.fk_article
+    GROUP BY A.id_article ORDER BY ${sort_by};
+    `;
+}
+
+// Função para lidar com a lógica de rota
+function handle_route(req, res, sort_by) {
+    const sql = generate_SQL(sort_by);
+
+    mysql.query(sql, (err, result) => {
+        if (err) throw err;
+
+        res.render('home', {
+            data: result
+        });
+    });
+}
+
+// Rotas definidas usando funções modularizadas
+router.get('/comentados', (req, res) => {
+    handle_route(req, res, 'article_comments DESC');
+});
+
+router.get('/recentes', (req, res) => {
+    handle_route(req, res, 'diff_secs DESC');
+});
+
+router.get('/', (req, res) => {
+    handle_route(req, res, 'A.article_likes DESC');
+});
+
+module.exports = router;
